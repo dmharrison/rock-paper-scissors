9844678f70964a245943bb85fb9a141b2e54a133
dmharrison <dmharrison2014@gmail.com>
2024-07-23 13:07:23 -0400

    finished game logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +25,29 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+game_images=[rock, paper, scissors]
+
+user_choice= int(input("what do you choose? Type 0 for Rock, 1, for Paper or 2 for Scissors.\n"))
+print("You chose:")
+if user_choice >=3 or user_choice < 0:
+    print("not a valid number you lose")
+else:
+    print(game_images[user_choice])
+
+    computer_choice= random.randint(0,2)
+    print(f"Computer chose:")
+    print(game_images[computer_choice])
+
+    choices = [rock, paper, scissors]
+
+    if user_choice == 0 and computer_choice == 2:
+        print("you win")
+    elif computer_choice == 0 and user_choice == 2:
+        print("you lose")
+    elif user_choice < computer_choice:
+        print("you lose")
+    elif user_choice > computer_choice:
+        print("you win")
+    elif user_choice == computer_choice:
+        print("it's a draw")
